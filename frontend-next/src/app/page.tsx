e2a4818c4dd8809821ac351e63c1a267123b88b6
kasemsan00 <kasemsan.cho@gmail.com>
2023-12-06 10:41:05 +0700

    home

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <main className="bg-gray-600 flex justify-center items-center h-screen">
-      <div className="text-4xl font-bold text-white">Video Conference</div>
+    <main>
+      <div className="bg-gray-800 flex justify-center items-center h-screen text-4xl font-bold text-white">Video Conference</div>
     </main>
   );
 }
--- frontend-next/src/app/r/[room]/page.tsx
+++ frontend-next/src/app/r/[room]/page.tsx
@@ -34,7 +34,7 @@ export default function Room() {
         <PreJoin onSubmit={handlePreJoin} />
       ) : (
         <>
-          <Chat />
+          {/* <Chat /> */}
           <VideoConference />
           <RoomAudioRenderer />
         </>
