aafe12979bb1cc64dcda6b61a565f47c2e652062
kasemsan00 <kasemsan.cho@gmail.com>
2023-12-06 10:34:02 +0700

    home

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,3 +1,9 @@
+import "./globals.css";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-gray-600 flex justify-center items-center h-screen">
+      <div className="text-4xl font-bold text-white">Video Conference</div>
+    </main>
+  );
 }
